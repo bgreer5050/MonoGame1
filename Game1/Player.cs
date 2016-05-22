53d9806fcf4c84b9c54143b1b6234222728ef4f2
Ben Greer <Ben Greer>
2016-05-22 13:53:53 -0500

    Added some properties to the player class

--- Game1/Player.cs
+++ Game1/Player.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Game1
 {
     public class Player
     {
+
+        public Texture2D PlayerTexture; //Animation representing the player
+
+        public Vector2 Position; //Position of player
+
+        public bool Active; //State of Player
+
+        public int Health; 
+
+        //Get the width of the player
+        public int Width
+        {
+            get { return PlayerTexture.Width; }
+        }
+
+        //Get player height
+        public int Height
+        {
+            get { return PlayerTexture.Height; }
+        }
+
         public void initialize()
         {
 
